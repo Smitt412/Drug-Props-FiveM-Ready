f99d36134e3f80c35188d3ebf35c59f3e5581e29
Smitt412 <Lamar5503@me.com>
2024-08-10 03:11:58 -0400

    Add files via upload

---
+++ Smit_DrugProps/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game { 'gta5' }
+name "Smitt Drug Props"
+author 'Smitty412 https://github.com/Smitt412'
+description 'Fivem Ready Drug Props'
+
+lua54 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/smit_drugprops.ytyp'
